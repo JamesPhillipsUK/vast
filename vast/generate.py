6d92530614179469d6c1c4687e88e46f96ca5c8c
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2022-03-15 18:15:46 +0000

    Split vast.py into generate.py and visualise.py.

---
+++ vast/generate.py
@@ -0,0 +1,71 @@
+"""generator.py Generates an Abstract Syntax Tree
+
+VAST - Building and visualising Abstract Syntax Trees for Python code.
+
+Authors: Jesse Phillips <james@jamesphillipsuk.com>
+
+"""
+import ast
+import urllib.request
+
+
+def generateASTFromURL(uRL: str):
+    """This method, given a python file's URL, generates an abstract
+    syntax tree for it.
+
+    Args:
+        uRL (string): The URL.
+
+    Returns:
+        AST: The abstract syntax tree.
+
+    """
+    if uRL:
+        try:
+            fileString = urllib.request.urlopen(uRL).read().decode()
+        except Exception as E:
+            raise E
+        try:
+            aST = ast.parse(fileString)
+        except SyntaxError as S:
+            raise S
+        return aST
+    raise ValueError("Cannot generate AST from URL if none is provided.")
+
+
+def generateASTFromPath(path: str):
+    """This method, given a python file's path, generates an abstract
+    syntax tree for it.
+
+    Args:
+        path (string): The filepath.
+
+    Returns:
+        AST: The abstract syntax tree.
+
+    """
+    with open(path) as file:
+        fileString = file.read()
+    if fileString:
+        aST = ast.parse(fileString)
+        return aST
+    else:
+        raise ValueError("Cannot make an AST from an empty file.")
+
+
+def generateASTFromString(codeString: str):
+    """This method, given a string of Python, generates an abstract
+    syntax tree for it.
+
+    Args:
+        codeString (string): The code to analyse.
+
+    Returns:
+        AST: The abstract syntax tree.
+
+    """
+    if codeString:
+        aST = ast.parse(codeString)
+        return aST
+    else:
+        raise ValueError("Cannot make an AST from an empty string.")
---
+++ vast/visualise.py
@@ -0,0 +1,74 @@
+"""visualise.py Visualises an Abstract Syntax Tree
+
+VAST - Building and visualising Abstract Syntax Trees for Python code.
+
+Authors: Jesse Phillips <james@jamesphillipsuk.com>
+
+"""
+
+import ast
+import sys
+import networkx as nx
+import EoN
+import matplotlib.pyplot as plt
+
+
+def visualiseASTGraph(aST: ast):
+    """Builds a visualisation of the provided AST.
+
+    Args:
+        aST (AST): The abstract syntax tree.
+
+    """
+    GRAPH = nx.DiGraph()
+    rootNodeID = "noRoot"
+    edges = []
+    labelDictionary = {}
+
+    # Walk the tree, breadth-first, noting all edges.
+    for node in ast.walk(aST):
+        nodeID = str(node.__class__) + str(id(node))  # Unique name
+        nodeLabel = str(node.__class__).split("ast.")[1].split("'>")[0]
+        if nodeLabel == "Constant":
+            nodeLabel += " " + str(node.value)
+        elif nodeLabel == "FunctionDef":
+            nodeLabel += " " + str(node.name)
+        labelDictionary[nodeID] = nodeLabel
+
+        if rootNodeID == "noRoot":
+            rootNodeID = nodeID
+
+        for child in ast.iter_child_nodes(node):
+            childNodeID = str(child.__class__) + str(id(child))
+            for edge in edges:
+                if edge[1] == childNodeID:
+                    childNodeID += str(1)  # IDs aren't unique.  Fix.
+
+            # If child is at the bottom of the tree, it won't get walked.
+            # Label it manually.
+            if labelDictionary.get(childNodeID) is None:
+                childLabel = str(child.__class__
+                                 ).split("t.")[1].split("'>")[0]
+                if childLabel == "Constant":
+                    childLabel += " " + str(child.value)
+
+                labelDictionary[childNodeID] = childLabel
+                if (childLabel == "Load"
+                   or childLabel == "Store"
+                   or childLabel == "Del"):
+                    if hasattr(node, "id"):
+                        nodeLabel = str(node.id)
+                        labelDictionary[nodeID] = nodeLabel
+
+            GRAPH.add_edge(nodeID, childNodeID)
+            edges.append([nodeID, childNodeID])
+
+    # Make the graph look like a tree using hierarchy_pos.
+    pos = EoN.hierarchy_pos(GRAPH, rootNodeID)
+    nx.draw_networkx_nodes(GRAPH, pos=pos, alpha=0.6)
+    nx.draw_networkx_edges(GRAPH, pos=pos, alpha=0.5)
+    nx.draw_networkx_labels(GRAPH, pos=pos, labels=labelDictionary)
+
+    plt.tight_layout(pad=0)
+    plt.axis("off")
+    plt.show()  # Tada!
