f0d12d040c6ec5779d75ee74481f186bdc6ca5c8
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2023-11-09 20:04:29 +0000

    Added node colours to modules, classes, and functions.  visualise.graph split into methods.

--- visast/visualise.py
+++ visast/visualise.py
@@ -13,12 +13,11 @@ import EoN
 import matplotlib.pyplot as plt
 
 
-def graph(aST: ast):
-    """Builds a visualisation of the provided AST.
+def graph(aST: ast) -> None:
+    """ Builds a visualisation of the provided AST.
 
     Args:
         aST (AST): The abstract syntax tree.
-
     """
     GRAPH = nx.DiGraph()
     rootNodeID = "noRoot"
@@ -62,12 +61,48 @@ def graph(aST: ast):
 
             GRAPH.add_edge(nodeID, childNodeID)
             edges.append([nodeID, childNodeID])
+    __plotGraph(GRAPH, rootNodeID, labelDictionary)
 
+
+def __colourNodes(labels: dict) -> list:
+    """ Private.  Colours important nodes in an AST.
+    For python 3.9 support, this uses an if statement.  It would be more
+    efficient in 3.10+ to use a match/case block.
+
+    Args:
+        labels(dict(str)): the labels for the AST.  Search these for features.
+    
+    Returns:
+        list: the list of colours for nodes in the graph.
+    """
+    colourMap = []
+    for label in labels:
+        l = label.split("ast.")[1].split("'>")[0]
+        if l == "Module":
+            colourMap.append("#b3ffb3")
+        elif l == "ClassDef":
+            colourMap.append("#ffffb3")
+        elif l == "FunctionDef":
+            colourMap.append("#ffb3ff")
+        else:
+            colourMap.append("#1f78b4")
+    return colourMap
+
+
+def __plotGraph(graph: nx.DiGraph, rootNodeID: str, labels: dict) -> None:
+    """ Private.  Plots a given networkx DiGraph as an AST.
+
+    Args:
+        graph (nx.DiGraph): the graph to plot.
+        rootNodeID (string): the root node of the graph represented as a tree.
+        labels (dict(string)): the labels for the nodes in the graph.
+    """
     # Make the graph look like a tree using hierarchy_pos.
-    pos = EoN.hierarchy_pos(GRAPH, rootNodeID)
-    nx.draw_networkx_nodes(GRAPH, pos=pos, alpha=0.6)
-    nx.draw_networkx_edges(GRAPH, pos=pos, alpha=0.5)
-    nx.draw_networkx_labels(GRAPH, pos=pos, labels=labelDictionary)
+    pos = EoN.hierarchy_pos(graph, rootNodeID)
+    colourMap = __colourNodes(labels)
+    nx.draw_networkx_nodes(graph, pos=pos, alpha=0.6, node_color=colourMap)
+    nx.draw_networkx_edges(graph, pos=pos, alpha=0.5)
+    nx.draw_networkx_labels(graph, pos=pos, labels=labels)
 
     plt.title("Abstract Syntax Tree:")
     plt.tight_layout(pad=0)
